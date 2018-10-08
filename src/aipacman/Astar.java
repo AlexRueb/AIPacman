ec42f9a3bb2523299c4731546bc5b736ed8bfdc5
root <root@localhost.localdomain>
2018-10-07 19:31:28 -0600

    Stuff

--- src/aipacman/Astar.java
+++ src/aipacman/Astar.java
@@ -2,8 +2,6 @@
 package aipacman;
 
 import java.util.Stack;
-import java.lang.Math;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Astar extends InformedAgent{
@@ -22,19 +20,19 @@ public class Astar extends InformedAgent{
     private int evaluation_function(Node target){
         return manhattan_distance(target, root);
     }
-    
     @Override
-    public void findParent(Node target, char c){
+    public void findParent(Node target){
         if(target.parent != null){
             answer.push(target.parent);
             pathCost++;
-            findParent(target.parent, c);
+            target.id = '.';
+            findParent(target.parent);
         }
     }
     
     public int path_cost(Node n){
         pathCost = 0;
-        findParent(n, 'F');
+        findParent(n);
         return pathCost;
     }
     
@@ -57,32 +55,34 @@ public class Astar extends InformedAgent{
         //determine which node to expand using heuristic function
         frontier.add(root);
         root.visited = true;
-        int step = 0;
         while (!frontier.isEmpty()) {
             nodesExpanded++;
             //always expand node that has lowest manhattan score to goal
             Node current = frontier.poll();
             if(current.id == '*'){
                 answer.push(current);
-                super.findParent(answer.pop(), '*');
+                super.findParent(answer.pop());
+                stepsTaken = pathCost;
                 System.out.println("Found the path!");
                 return maze;
-            } else {
-                for(Node n : current.neighbors){
-                    //print_board();
-                    if(n == null) continue;
-                    n.estimated_cost = manhattan_distance(n, goal) + path_cost(n);
-                    
-                    //If neighbor of current node has been visited
-                    if(n.visited){continue;} 
-                    else {
-                       n.visited = true;
-                       frontier.add(n);
-                    }
-                    n.parent = current;
+            }
+            for(Node n : current.neighbors){
+                //print_board();
+                if(n == null) continue;
+                
+                int tentative_score = path_cost(current);
+
+                //If neighbor of current node has been visited
+                if(n.visited){
+                    frontier.add(n);
+                } 
+                else if(tentative_score >= n.distance_to_goal){
+                   continue;
                 }
-                current.visited = true;
-            }  
+                n.parent = current;
+                n.distance_to_start = tentative_score;
+                n.estimated_cost = n.distance_to_start + heuristic_function(n);
+            }
         }
         
         return maze;
